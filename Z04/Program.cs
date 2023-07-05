6d5fa98ccd3ce0753a0e6b8e11b51c1d6d44c3d6
Anton Gusev <antongusev705@gmail.com>
2023-07-05 15:43:55 +0300

    задание 8

---
+++ Z04/Program.cs
@@ -0,0 +1,25 @@
+﻿ int i = 1;
+            bool not = true;
+
+            Console.WriteLine("Введите число:");
+         int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Чётные числа от 1 до  " + A);
+            while (i <= A)
+            {
+                if (i % 2 != 1)
+                {
+                    Console.Write(i + ", ");
+                    not = false;
+                }
+                i++;
+            }
+
+            if (not)
+            {
+                Console.WriteLine("Нет чётных чисел!");
+            }
+          
+
+     
+          
