7dc06c15cdb65517a579489715dad4d4a58f40af
Anton Gusev <antongusev705@gmail.com>
2023-07-05 15:29:07 +0300

    задание6

---
+++ Z03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+         int A = Convert.ToInt32(Console.ReadLine());
+
+            if (A % 2 == 1)
+            {
+                Console.WriteLine("Число " + A + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + A + "является: ЧЁТНЫМ");
+            }
